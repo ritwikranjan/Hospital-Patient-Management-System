12c367633de6ecf5b27d8f3c616e56ead2282a46
kanishksoni <42889372+kanishksoni@users.noreply.github.com>
2020-10-11 16:22:43 +0530

    Images added and Database connected

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const mongoDBUri = 'mongodb+srv://Soni_kanishk:password@hpmsdb.jcmbh.mongodb.net/hpmsDB?retryWrites=true&w=majority'
+
+mongoose.connect(mongoDBUri || 'mongodb://localhost/hpms',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected',()=>{
+    console.log("Mongoose is connected!!!");
+});
+
 app.use(morgan('tiny'));
 
 app.get('/api',(req, res) => {
